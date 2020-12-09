c40a57d7f8a171b5fb403f07fa9ab9bf3a623a38
Dima <dima@gmail.com>
2020-12-09 17:10:51 +0200

    Added stickman animation, win and loose logic

--- dist/app.js
+++ dist/app.js
@@ -14,7 +14,7 @@ function createLetterButtons() {
 
 
 
-const getRandomWord = () => { return words[Math.floor((Math.random() * words.length) + 1)]}
+const getRandomWord = () => { return words[Math.floor((Math.random() * words.length) + 1)] }
 
 function createWordDashes() {
     const wordEl = document.querySelector('.game__word');
@@ -32,10 +32,9 @@ function createWordDashes() {
 
 function check() {
     const currentLetter = this.innerText;
-    if(word.includes(currentLetter)) {
+    if (word.includes(currentLetter)) {
         this.classList.add('correct-guess');
         const dashesArray = document.querySelectorAll('.word-letter');
-        //dashesArray[word.indexOf(currentLetter)].innerHTML = currentLetter;
         for (let i = 0; i < word.length; i++) {
             if (word[i] === currentLetter) {
                 console.log('yes');
@@ -44,28 +43,28 @@ function check() {
         }
     } else {
         this.classList.add('wrong-guess');
-    }    
+    }
 }
 
 function play() {
     createLetterButtons();
     const gameButtons = document.querySelectorAll('.letter');
-    gameButtons.forEach(button => { button.addEventListener('click', check)})
+    gameButtons.forEach(button => { button.addEventListener('click', check) })
     console.log(word);
     createWordDashes();
 }
 
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-const words = ["alderaan", "asteroid", "coruscant", "dagobah", "hoth", "kashyyyk", "naboo", "tatooine", "droid", "atat", "blaster", 
-"podracer", "sandcrawler", "clone", "luke", "leia", "vader", "biggs", "dooku", "dak", "emperor", "palpatine", "fin", "rey", "tarkin", 
-"han", "chewbacca", "jabba", "greedo", "lando", "yoda", "padme", "maul", "wedge", "yoda", "bantha", "sarlacc", "wampa", "tauntaun", 
-"wookie", "empire", "rebel", "light", "dark", "bowcaster", "phasma", "lightsaber", "skywalker", "sith", "jedi"];
+const words = ["alderaan", "asteroid", "coruscant", "dagobah", "hoth", "kashyyyk", "naboo", "tatooine", "droid", "atat", "blaster",
+    "podracer", "sandcrawler", "clone", "luke", "leia", "vader", "biggs", "dooku", "dak", "emperor", "palpatine", "fin", "rey", "tarkin",
+    "han", "chewbacca", "jabba", "greedo", "lando", "yoda", "padme", "maul", "wedge", "yoda", "bantha", "sarlacc", "wampa", "tauntaun",
+    "wookie", "empire", "rebel", "light", "dark", "bowcaster", "phasma", "lightsaber", "skywalker", "sith", "jedi"];
 
 const word = getRandomWord();
 console.log(word);
-window.onload = function() {
+window.onload = function () {
     play();
 }
